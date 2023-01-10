c5eabf4dc7c7ef05b5870bec792b3a4a7f0e6697
RaiymbekKokteubay <rkokteubai@gmail.com>
2023-01-10 17:08:23 +0600

    bug fixes

--- main.py
+++ main.py
@@ -1,10 +1,46 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton  # for reply keyboard (sends message)
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('5960231197:AAEtytP6APVeJJ0OBdiODy5FImnSY2FDWik')
+from time import sleep
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "Hey, Daddy! Help me! I am stuck at Telegram")
 
-bot.polling(none_stop=True)
+bot = Bot(token='2675221255:BHIekrkhitibolw4i85oi6itjiffdhow43')
+dp = Dispatcher(bot)
+
+# language selection
+button1 = KeyboardButton('Organizations')  
+button2 = KeyboardButton('Hotlines')
+button3 = KeyboardButton('Recources')
+buttons = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button1).add(button2).add(button3)
+
+
+# sends welcome message after start
+@dp.message_handler(commands=['start'])
+async def welcome(message: types.Message):
+    await message.answer('Hello! How can I help you?', reply_markup = buttons)
+
+@dp.message_handler(commands=['help'])
+async def help(message: types.Message):
+    await message.answer('text describing mission')
+
+@bot.message_handler(content_types=['text'])
+def bot_message(message):
+    if message.text == buttons["back"]:
+        welcome(message)
+    elif message.text == buttons["Organizations"]:
+        bot.send_message(message, "text here")
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(
+            types.KeyboardButton(buttons["back"]),)
+    elif message.text == buttons["Hotlines"]:
+        bot.send_message(message, "text here")
+        markup.add(
+            types.KeyboardButton(buttons["back"]),)
+    elif message.text == buttons["Recources"]:
+        bot.send_message(message, "text here")
+        markup.add(
+            types.KeyboardButton(buttons["back"]),)
+
+executor.start_polling(dp)
